8df52c498a2bc9ff51810779db9f785581c64241
akhilsiraswal <akhilsiraswal@gmail.com>
2021-06-09 09:33:48 +0530

    work

---
+++ shopping-app/navigation/ShopNavigation.js
@@ -0,0 +1,24 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Colors from "../constants/Colors";
+import ProductsOverviewScreen from "../screens/shop/ProductOverviewScreen";
+
+import { Platform } from "react-native";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.accent,
+    },
+  }
+);
+
+export default createAppContainer(ProductsNavigator);
